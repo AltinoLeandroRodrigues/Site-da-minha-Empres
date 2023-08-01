ed9f3c26c04d47d0e928855c1da28f86096e0c40
Altino Leandro Rodiguês <136701250+AltinoLeandroRodrigues@users.noreply.github.com>
2023-08-02 00:45:40 +0100

    email

--- email.js
+++ email.js
@@ -1,1 +1,21 @@
+function modal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "flex";
+
+  setTimeout(function() {
+    fecharModal(modal);
+  }, 1000);
+
+
+// Função para fechar a modal
+function fecharModal(modal) {
+  modal.style.display = "none";
+}
+}
+
+
+
+
+
+ 
  
